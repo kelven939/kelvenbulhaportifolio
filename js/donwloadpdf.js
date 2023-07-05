b7f08666309a4180793e9b8235a7e2c4afc49f63
Kelven Bruno Carlos Lopes Bulha (KB) <112175768+kelven939@users.noreply.github.com>
2023-07-05 17:10:35 +0430

    update script

--- js/donwloadpdf.js
+++ js/donwloadpdf.js
@@ -11,3 +11,18 @@ downloadButton.addEventListener('click', () => {
     };
     html2pdf().set(options).from(pdfgenarar).save();
 });
+
+
+const downloadButton1 = document.querySelector('#downloadButton1');
+downloadButton.addEventListener('click', () => {
+
+    const sobreme = document.querySelector('.sobreme');
+
+    const options = {
+        margin: [10, 10, 10, 10],
+        filename: "protfolio.pdf",
+        html2canvas: { scale: 2 },
+        jsPDF: {unit: "mm", format: "a4", orientation: "portrait"},
+    };
+    html2pdf().set(options).from(sobreme).save();
+});
